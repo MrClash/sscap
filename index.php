d93c34cf129efd8b2ec3cd32617afb8a62cb83ce
MrClash <superclash806@gmail.com>
2016-08-02 08:27:51 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-define('API_KEY','XXXX');
-$the_admin_id = 0;
+define('API_KEY','238011302:AAHCK9X70GS8e6ME-fCuEGru9Cslq11h2l0');
+$the_admin_id = 195651268;
 
 file_put_contents("count",file_get_contents("count")+1);
 
